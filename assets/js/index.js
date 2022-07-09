76c64252efa37dedacbdc7fb6ddc555d58434cd0
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-07-09 15:08:38 +0300

    Creating фctor сards: сustomizing сards - JS сode

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,38 @@
+const socialMap = new Map();
+socialMap.set("www.facebook.com", "fa-facebook-f");
+socialMap.set("twitter.com", "fa-twitter");
+socialMap.set("www.instagram.com", "fa-instagram");
+
+const listActors = [];
+
 fetch("./assets/js/data.json")
   .then((resolve) => resolve.json())
   .then((actors) => {
-    const HTMLCollectionActors = actors.map((actor) => createActorCard(actor));
+    const HTMLCollectionActors = actors
+      .filter((actor) => actor.firstName || actor.lastName)
+      .map((actor) => createActorCard(actor));
 
     actorsBlock.append(...HTMLCollectionActors);
+  })
+  .catch(() => {
+    const errorText = createElement(
+      "p",
+      {
+        classNames: ["error-text"],
+      },
+      document.createTextNode(
+        "Sorry, there was a technical problem, please try again later!"
+      )
+    );
+
+    const error = createElement("div", { classNames: ["error"] }, errorText);
+
+    const errorOpacity = createElement("div", {
+      classNames: ["error-opacity"],
+    });
+
+    document.body.style.overflow = "hidden";
+    document.body.append(errorOpacity, error);
   });
 
 function createActorCard({
@@ -13,6 +42,41 @@ function createActorCard({
   profilePicture,
   contacts,
 }) {
+  const actorSocialCollection = contacts.map((item) => {
+    const hostNameLink = new URL(item).hostname;
+    return createElement(
+      "div",
+      {
+        classNames: ["actor-social"],
+      },
+      createElement("a", {
+        classNames: [
+          "actor-social-link",
+          "fa-brands",
+          socialMap.get(hostNameLink),
+        ],
+        attributes: { href: item, target: "_blank" },
+      })
+    );
+  });
+
+  const actorSocials = createElement(
+    "div",
+    {
+      classNames: ["actor-socials"],
+    },
+    ...actorSocialCollection
+  );
+
+  const actorFIO = createElement(
+    "h2",
+    {
+      classNames: ["actor-fio"],
+      attributes: { id: `actor-fio-${id}` },
+    },
+    document.createTextNode(`${firstName} ${lastName}`)
+  );
+
   const actorInitials = createElement(
     "div",
     {
@@ -59,12 +123,30 @@ function createActorCard({
 
   const actor = createElement(
     "article",
-    { classNames: ["actor"] },
-    actorAvatarBlock
+    {
+      classNames: ["actor"],
+      attributes: { "data-actor-id": `actor-fio-${id}` },
+      events: { click: addActorToList },
+    },
+    actorAvatarBlock,
+    actorFIO,
+    actorSocials
   );
 
   return actor;
 }
+
+const actorListSpan = createElement(
+  "span",
+  {},
+  document.createTextNode("You choosed")
+);
+
+const actorList = createElement(
+  "ul",
+  { classNames: ["actors-list"] },
+  actorListSpan
+);
 
 const actorsBlock = createElement("div", { classNames: ["actors-block"] });
 
@@ -78,7 +160,8 @@ const actorsInner = createElement(
   "div",
   { classNames: ["actors-inner"] },
   actorsTitle,
-  actorsBlock
+  actorsBlock,
+  actorList
 );
 
 const container = createElement(
@@ -91,16 +174,44 @@ const actors = createElement("section", { classNames: ["actors"] }, container);
 
 document.body.append(actors);
 
-// ____________________________________________
 
+
+
+// Функции и Оброботчики событий
+
+//Обработчик на добавление актера в список
+function addActorToList({ target }) {
+  if (!document.getElementById(target.dataset.actorId)) {
+    return;
+  } else {
+    const fioCurrentActor = document.getElementById(
+      target.dataset.actorId
+    ).textContent;
+
+    if (listActors.includes(fioCurrentActor)) {
+      return;
+    } else {
+      actorListSpan.style.display = "none";
+
+      listActors.push(fioCurrentActor);
+      const actorItem = createElement(
+        "li",
+        { classNames: ["actor-item"] },
+        document.createTextNode(fioCurrentActor)
+      );
+      actorList.append(actorItem);
+    }
+  }
+}
+// Обработчик на ошибку
 function handlerError({ target }) {
   target.remove();
 }
-
+// Обработчик на загрузку
 function handlerLoadPhoto({ target }) {
   document.getElementById(target.dataset.wrapperId).append(target);
 }
-
+//Функция для создания любого элемента, его классов, стилей, атрибутов, событий и добавления в DOM
 function createElement(
   tag,
   { classNames = [], styles = {}, attributes = {}, events = {} },
@@ -128,7 +239,7 @@ function createElement(
 
   return element;
 }
-
+//Функция для трансформации строки в цвет
 function stringToColour(str) {
   let hash = 0;
   for (let i = 0; i < str.length; i++) {
@@ -141,7 +252,7 @@ function stringToColour(str) {
   }
   return colour;
 }
-
+//Функция для трасформация имени и фамилии в инициалы
 function createInitials(str) {
   return str
     .split(" ")
